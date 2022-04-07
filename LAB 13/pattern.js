543a921f541f75dc11155d9e30bb2c545807a050
20IT120-JIMMY <80442931+20IT120-JIMMY@users.noreply.github.com>
2022-04-07 10:51:07 +0530

    Update pattern.js

--- LAB 13/pattern.js
+++ LAB 13/pattern.js
@@ -1,7 +1,7 @@
 function WithPattern() {
-    let pattern = /W.d[a-z]+y/;
+    let pattern = /J.m[a-z]+y/;
     alert(
-      "Pattern is First char is M and third char is d and last char is y."
+      "Pattern is First char is J and third char is m and last char is y."
     );
     let word = prompt("Enter any word");
     if (pattern.test(word)) alert("Pattern is correct");
